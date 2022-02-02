f0ceff8dd1495774f0752501bd305df8d792d2fb
Fernandeeess <82266442+Fernandeeess@users.noreply.github.com>
2022-02-02 13:49:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ let imgpa = document.getElementById("imgpa");
 
 console.log("Agora sao exatamente " + hora + ":" + minutos + " horas");
 if (hora > 5 && hora < 12) {
-    imgpa.src = "//imagens/Morning.jpg";
+    imgpa.src = "imagens/Morning.jpg";
     paragrafo.innerHTML = "Agora são " + hora + ":" + minutos + " da manha";
     document.body.style.backgroundColor = "#FAA136";
     console.log("Bom Dia!");
 } else if (hora >= 12 && hora < 18) {
 
-    imgpa.src = "//imagens/evening.jpg";
+    imgpa.src = "imagens/evening.jpg";
     paragrafo.innerHTML = "Agora são " + hora + ":" + minutos + " da tarde";
     document.body.style.backgroundColor = "#FAB106";
     console.log("Boa Tarde!");
 
 } else if (hora > 18 && hora <= 24) {
-    imgpa.src = "//imagens/night.jpg"
+    imgpa.src = "imagens/night.jpg"
     paragrafo.innerHTML = "Agora são " + hora + ":" + minutos + " da noite";
     document.body.style.backgroundColor = "#262C3B";
     console.log("Boa  Noite!");
@@ -30,7 +30,7 @@ if (hora > 5 && hora < 12) {
 
     paragrafo.innerHTML = "Agora são " + hora + ":" + minutos + " da madrugada";
     document.body.style.backgroundColor = "#0E163B";
-    imgpa.src = "//imagens/dawn.jpg";
+    imgpa.src = "imagens/dawn.jpg";
     console.log("Boa Madrugada");
 
 
